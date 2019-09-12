edc51952c0c31c72a7de2616801d879cbf23b585
王宇逸 <Strawberry_Str@hotmail.com>
2019-09-12 20:55:23 +0800

    Add WPF sample.

---
+++ MessageRoutine.Sample.Wpf/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MessageRoutine.Sample.Wpf
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MessageRoutine.Sample.Wpf/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MessageRoutine.Sample.Wpf
+{
+    public class Program
+    {
+        public static readonly MessageManager Manager = new MessageManager();
+
+        [STAThread]
+        public static int Main() => Manager
+            .RegisterSingleton<App>()
+            .RegisterService<Program>()
+            .StartRoutine<int>("Program.Startup");
+
+        [Inject]
+        protected App App { get; set; }
+
+        [Message("Program.Startup")]
+        public Routine Startup() => new Routine(null, App.Run());
+    }
+}
